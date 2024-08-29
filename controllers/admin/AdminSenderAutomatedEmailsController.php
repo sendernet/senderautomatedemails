116201afd65d0a8698101f52e50d83444159f4c7
dimrak22 <diego@sender.net>
2024-08-29 17:18:49 +0300

    remove commented

--- controllers/admin/AdminSenderAutomatedEmailsController.php
+++ controllers/admin/AdminSenderAutomatedEmailsController.php
@@ -135,10 +135,6 @@ class AdminSenderAutomatedEmailsController extends ModuleAdminController
         $this->module->apiClient = new SenderApiClient();
         $this->module->apiClient->setApiKey(Configuration::get('SPM_API_KEY'));
 
-//        if (!$this->module->apiClient->checkApiKey()) {
-//            $this->disconnect();
-//        }
-
         if (version_compare(_PS_VERSION_, '1.6.0.0', '>=')) {
             $disconnectUrl = $this->context->link->getAdminLink('AdminSenderAutomatedEmails') . '&disconnect=true';
         } else {
